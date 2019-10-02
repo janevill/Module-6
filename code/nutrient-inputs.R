e76d9756571555d61d5ae364a4313616dbd57365
janevill <janevill@ncsu.edu>
2019-10-01 22:44:57 -0400

    class coding exercises

--- code/nutrient-inputs.R
+++ code/nutrient-inputs.R
@@ -21,17 +21,67 @@ library(tidyverse)
 
 # Read & inspect the dataset ----
 nut <- read_csv("data/nutrient-inputs-reformatted.csv")
-
+str(nut)
+view(nut)
+summary(nut)
 ## Tidy data ----
-# 1. Gather and separate columns
+# 1. Gather and separate columns----
 
-# 2. Convert year to numeric
-
-# 3. Handle NAs
-
-# 4. Create a NC subset
+tidy_nut <- nut %>%
+  gather(-State,-County,-FIPS,-Area,
+         key = "year_nut_application",
+         value="kg") %>%
+  separate(year_nut_application, 
+           into= c("year", "nut","application"),
+           sep= "_") ->tidy_nut
   
+
+
+# 2. Convert year to numeric----
+
+tidy_nut$year <- as.numeric(tidy_nut$year)
+
+# 3. Handle NAs-----
+tidy_nut %>%
+  drop_na()
+tidy_nut %>%
+  drop_na(kg)
+tidy_nut %>%
+  drop_na(Area)
+
+##fill() - fills NAs with most recent value
+tidy_nut %>%
+  fill(kg, .direction = "down")
+tidy_nut %>%
+  fill(kg, .direction = "up")
+
+##replace_na- specify value to fill in for nAs
+tidy_nut %>%
+  replace_na(list(kg=0))
+
+# 4. Create a NC subset-----
+tidy_nut %>%
+  filter(State == "NC") ->NC_nut
+head(NC_nut)
+
 # Visualize ----
+NC_nut %>%
+  filter(County == "WAKE") %>%
+  ggplot(mapping=aes(x=year,y=kg,color = application)) +
+  geom_point()+
+  geom_line()+
+  theme_minimal()+
+  facet_wrap(~ nut,ncol=1, 
+             scales= "free_y")
+
 
 # Summarize ----
 ### Which NC counties, on average over the period of record, have the highest rates of N and P input relative to different application types?
+NC_nut %>%
+  group_by(County,nut,application) %>%
+  summarize(mean_kg = mean(kg)) %>%
+  ungroup()%>%
+  group_by(nut, application) %>%
+  mutate(rank=min_rank(desc(mean_kg))) %>%
+  arrange(rank)
+
